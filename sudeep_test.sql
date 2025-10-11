f4c8c2a87c3b36aaaef25b1b1d3cb12061e86639
unknown <sudeepyalasangi@gmail.com>
2025-10-12 00:44:26 +0530

    this is my SQL file

---
+++ sudeep_test.sql
@@ -0,0 +1,27 @@
+-- 05. Get the top 3 loyal customers
+
+-- SELECT * FROM customers
+-- ORDER  BY  points DESC
+-- LIMIT 3;
+
+-- 01. Last name start with MY or contains SE 
+--  SELECT * FROM customers
+--  WHERE last_name LIKE "MY%";
+ 
+--  12. Lat name ends with EY or ON
+-- SELECT * FROM customers
+-- WHERE last_name LIKE "%EY";
+ 
+ 
+--  13. Get the costomers whose phone numbers ends with 9
+ -- SELECT * FROM customers
+--  WHERE phone LIKE '%9';
+
+--  02. last name contains b followed by R or U
+    -- SELECT * FROM customers
+-- WHERE last_name REGEXP 'b[r]'; 
+
+
+
+
+
