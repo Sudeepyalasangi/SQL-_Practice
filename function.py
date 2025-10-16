4389f9f5d86475a80b1adbcfb41e1da10bb59cfa
unknown <sudeepyalasangi@gmail.com>
2025-10-16 15:48:56 +0530

    This is my function file

---
+++ function.py
@@ -0,0 +1,93 @@
+john = [1200,3000,5000,6000]
+print(john[0]+ john[1])
+
+john = [1200,3000,5000,6000]
+max = [675689,87960,54677,86787,76867]
+
+def expense(name):
+    total_expense = 0
+    for i in name:
+        total_expense =+ i
+    print("Total expense  :",total_expense)
+expense(john)   
+
+john = [1200,3000,5000,6000]
+max = [675689,87960,54677,86787,76867]
+
+total_expense = 0
+for i in max:
+   total_expense += i
+   print("Total expense  :",total_expense)
+
+def function_name():
+    ececution
+function_name()
+
+def msg():
+    print("hello world...")
+msg()
+
+def addition():
+    print(10+20)
+addition()
+n1 = int(inpout"Enter  a first number....")
+n2 = int(inpout"Enter  a second number....")
+
+def addition(a, b):
+    print(a + b)
+# addition(10,30)
+
+n1 = int(input("Enter  a first number...."))
+n2 = int(input("Enter  a second number...."))
+
+def addition(a, b):
+    print(a + b)
+addition(n1, n2)
+
+
+john = [1200,3000,5000,6000]
+max = [675689,87960,54677,86787,76867]
+
+def expense(name):
+    total_expense = 0
+    for i in name:
+        total_expense =+ i
+    print("Total expense  :",total_expense)
+expense(john)   
+
+
+john = [1200,3000,5000,6000]
+max = [675689,87960,54677,86787,76867]
+
+def expense(name):
+    total_expense = 0
+    for i in name:
+        total_expense =+ i
+    print("Total expense  :",total_expense)
+expense(max)   
+
+def addition(a, b=50):
+    print(a + b)
+addition(10,30)
+
+def addition(a, b=50):
+    print(a + b)
+addition(10)
+
+def addition(a=30, b=50):
+    print(a + b)
+
+addition(10)
+
+def addition(a=30, b=90):
+    print(a + b)
+addition(10)
+
+def addition(a=20,b=90):
+    return a + b
+a = addition () 
+print(a) 
+print(addition())
+
+
+
